28141b24c5e5e912ac13cf4b46927062c6973a0a
Ngumi22 <mwaurapngumi@gmail.com>
2024-02-20 21:41:32 +0300

    0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <stdbool.h>
 #include "sort.h"
 
 /**
@@ -9,27 +9,28 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j;
+	size_t i, len = size;
 	int temp;
-	int swapped;
+	bool swapped;
 
-	for (i = 0; i < size - 1; i++)
-	{
-		swapped = 0;
+	if (array == NULL || size < 2)
+		return;
 
-		for (j = 0; j < size - i - 1; j++)
+	do {
+		swapped = false;
+
+		for (i = 0; i < len - 1; i++)
 		{
-			if (array[j] > array[j + 1])
+			if (array[i] > array[i + 1])
 			{
-				temp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = temp;
+				temp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = temp;
 
-				swapped = 1;
 				print_array(array, size);
+				swapped = true;
 			}
 		}
-		if (swapped == 0)
-			break;
-	}
+		len--;
+	} while (swapped);
 }
